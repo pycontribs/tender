e5bf6b5399f08b20bbc6c7af115dcc8e02c014d6
Sorin Sbarnea <ssbarnea@redhat.com>
2020-08-16 19:13:35 +0100

    Add draft command

--- tender/__main__.py
+++ tender/__main__.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 """CLI Interface of Tender tool"""
+import datetime
 import json
 import logging
 import os
 import sys
 from copy import deepcopy
 from types import SimpleNamespace
+from typing import Dict, List, Optional
 
 import click
 import git
+import github
 import giturlparse
+import humanize
 import yaml
 from blessings import Terminal
 from click_help_colors import HelpColorsCommand, HelpColorsGroup, version_option
-from github import Github
+from packaging.version import parse as parse_version
 
 from tender import __version__
 
 term = Terminal()
 
 _logger = logging.getLogger(__name__)
+DEFAULT_EXCLUDE_LABELS = {"skip-changelog"}
 
 
 def link(url, name):
@@ -75,27 +80,72 @@ class Config(SimpleNamespace):
 
 
 class Tender:
+
+    # pylint: disable=too-many-instance-attributes
+
     def __init__(self, org, repo):
         self.cfg = Config()
+        self.git = git.Repo(".")
         self.repo_name = repo
         self.org_name = org
         token = os.environ.get("HOMEBREW_GITHUB_API_TOKEN")
-        self.github = Github(login_or_token=token)
+        self.github = github.Github(login_or_token=token)
         self.repo = self.github.get_repo(f"{org}/{repo}")
         self.pulls = self.repo.get_pulls(state="all")
+        self.required_labels = set()
+        self.errors = []
 
         # required_labels is a list of labels from which at least one needs to
         # be present on each PR, and only one.
-        self.required_labels = set()
-        if self.cfg.release_drafter["exclude-labels"]:
-            self.required_labels.update(self.cfg.release_drafter["exclude-labels"])
+
+        self.exclude_labels = set(
+            self.cfg.release_drafter.get("exclude-labels", DEFAULT_EXCLUDE_LABELS)
+        )
+
+        self.label_section_map = dict()
         for category in self.cfg.release_drafter["categories"]:
             if "labels" in category:
-                self.required_labels.update(category["labels"])
+                # self.required_labels.union(set(category["labels"]))
+                for label in category["labels"]:
+                    if label not in self.required_labels:
+                        self.required_labels.add(label)
+                    if label not in self.label_section_map:
+                        self.label_section_map[label] = category["title"]
             else:
                 _logger.warning(
                     "%s category does not have any labels defined.", category
                 )
+        _logger.info(
+            "Labels mapped to release notes sections: %s",
+            ", ".join(self.label_section_map.keys()),
+        )
+
+    def get_last_unreleased_tag(self) -> Optional[git.Tag]:
+        for tag in sorted(
+            self.git.tags, key=lambda t: t.commit.committed_datetime, reverse=True
+        ):
+            version = parse_version(str(tag))
+            if not version.is_prerelease:
+                _logger.info("Last non-prerelease tag was %s", version)
+                return tag
+        return None
+
+    def get_unreleased_commits(self) -> Dict[str, git.objects.commit.Commit]:
+        """Return list of commits since last *release* tag."""
+        result = {}
+        tag = self.get_last_unreleased_tag()
+        tagmap: Dict[str, List] = {}
+        for _ in self.git.tags:
+            tagmap.setdefault(self.git.commit(_), []).append(_)
+        # print(tagmap)
+        rev = f"{tag}..HEAD"
+        _logger.info("Looking for commits in %s range", rev)
+        for commit in self.git.iter_commits(rev=rev):
+            result[commit.hexsha] = commit
+        return result
+
+    def get_section_for_label(self, label):
+        return self.label_section_map[label]
 
     def do_pulls(self):
         _logger.info("Auditing pull-requests")
@@ -149,6 +199,80 @@ class Tender:
                     label.name,
                 )
 
+    def do_draft(self):
+        _logger.info("Draft release notes")
+
+        for release in self.repo.get_releases():
+            print(
+                f"tag_name={release.tag_name} name={release.title} draft={release.draft}"
+                f" prerelease={release.prerelease}"
+            )
+            if release.draft:
+                break
+
+        body = "## Changes\n\n"
+
+        commits = self.get_unreleased_commits()
+        sections = {
+            category["title"]: "" for category in self.cfg.release_drafter["categories"]
+        }
+
+        tag = self.get_last_unreleased_tag()
+        no_older_than = datetime.datetime.fromtimestamp(tag.commit.committed_date)
+
+        age = humanize.naturaltime(datetime.datetime.now() - no_older_than)
+        _logger.info(
+            "Counting %s commits since %s tag, made over %s.", len(commits), tag, age
+        )
+
+        for pull in self.repo.get_pulls(state="closed"):
+
+            if pull.merged:
+
+                # ignoring commits
+                labels = {x.name for x in pull.labels}
+                if not self.exclude_labels.isdisjoint(labels):
+                    continue
+
+                if pull.merge_commit_sha in commits:
+
+                    valid_labels = list(self.required_labels.intersection(labels))
+                    if valid_labels:
+                        section = self.label_section_map[valid_labels[0]]
+                        sections[section] = (
+                            f"{sections[section]}* {pull.title} (#{pull.number}) "
+                            f"@{pull.user.login}\n"
+                        )
+                    else:
+                        self.errors.append(
+                            "%s containes unknown labels %s, add one required labels %s."
+                            % (
+                                link(pull.html_url, "PR #{}".format(pull.number)),
+                                labels,
+                                self.required_labels,
+                            )
+                        )
+                elif pull.closed_at > no_older_than:
+                    print(pull.closed_at, no_older_than)
+                    _logger.warning(
+                        "Ignored %s because its commit %s was not found among unreleased commits.",
+                        pull.html_url,
+                        pull.merge_commit_sha,
+                    )
+                else:
+                    _logger.info(
+                        "Stopped processing PRs as we encounted first "
+                        "one merged before cutoff date."
+                    )
+                    break
+
+        for section, content in sections.items():
+            if content:
+                body += f"### {section}\n\n{content}\n"
+
+        release = None
+        print(body)
+
 
 def parsed(result):
     result.raise_for_status()
@@ -199,19 +323,34 @@ def cli(ctx, debug, repo, org):
     ctx.obj["app"] = Tender(org=org, repo=repo)
 
     if ctx.invoked_subcommand is None:
-        ctx.invoke(pulls)
-        ctx.invoke(labels)
+        # ctx.invoke(do_pulls)
+        # ctx.invoke(do_labels)
+        ctx.invoke(do_draft)
+
+    if len(ctx.obj["app"].errors) != 0:
+        for err in ctx.obj["app"].errors:
+            print(err)
+        sys.exit()
 
 
 @cli.command(cls=HelpColorsCommand)
 @click.pass_context
-def pulls(ctx):
+def do_draft(ctx):
+    """Generate release notes."""
+    ctx.obj["app"].do_draft()
+
+
+@cli.command(cls=HelpColorsCommand)
+@click.pass_context
+def do_pulls(ctx):
+    """Audit pull requests."""
     ctx.obj["app"].do_pulls()
 
 
 @cli.command(cls=HelpColorsCommand)
 @click.pass_context
-def labels(ctx):
+def do_labels(ctx):
+    """Check if correct labels are defined in project."""
     ctx.obj["app"].do_labels()
 
 
