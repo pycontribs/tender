df08641ab843f40d95b90170b45c7901748eadb3
Sorin Sbarnea <ssbarnea@redhat.com>
2020-08-16 21:42:31 +0100

    Fixed development versioning

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 """Kept for partial compatibility with old pip versions."""
 from setuptools import setup
 
-setup(name="tender", use_scm_version=True, setup_requires=["setuptools_scm"])
+setup(
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=["setuptools_scm[toml]>=3.5.0"],
+)
